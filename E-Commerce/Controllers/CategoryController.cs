1bc237c6ae4639dfb12ed637ea2aa2c2ad97656c
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-10-01 21:36:40 +0300

    Add project files.

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext(); 
+
+        public IActionResult Index()
+        {
+            var categories = dbContext.Categories.ToList();
+
+            return View(model: categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //Category category = new Category() { Name = categoryName };
+            dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ E-Commerce/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string name = "Mohamed Nabih From Controller";
+            List<string> list = new List<string>() { "Ahmed", "Ali", "Mona", "Mohamed" };
+
+            return View(model: list);
+        }
+    }
+}
